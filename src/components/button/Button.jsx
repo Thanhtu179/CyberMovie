dbb0386e0161960d245cf59da5a221adaab02ad1
Đinh Thanh Tú <89075138+Thanhtu179@users.noreply.github.com>
2022-01-16 19:56:14 +0900

    make button component and modal component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./button.scss";
+
+const Button = (props) => {
+  return (
+    <button
+      className={`btn ${props.className}`}
+      onclick={props.onclick ? () => props.onclick() : null}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+const OutLineButton = (props) => {
+  return (
+    <Button
+      className={`btn-outline ${props.className}`}
+      onclick={props.onclick ? () => props.onclick() : null}
+    >
+      {props.children}
+    </Button>
+  );
+};
+
+Button.propTypes = {
+  onclick: PropTypes.func,
+};
+
+export default Button;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -19,27 +19,27 @@ const headerNav = [
   },
 ];
 
-useEffect(() => {
-  const shrinkHeader = () => {
-    if (
-      document.body.scrollTop > 100 ||
-      document.documentElement.scrollTop > 100
-    ) {
-      headerRef.current.classList.add("shrink");
-    } else {
-      headerRef.current.classList.remove("shrink");
-    }
-  };
-  window.addEventListener("scroll", shrinkHeader);
-  return () => {
-    window.removeEventListener("scroll", shrinkHeader);
-  };
-}, []);
-
 const Header = () => {
   const { pathname } = useLocation();
   const headerRef = useRef(null);
   const active = headerNav.findIndex((e) => e.path === pathname);
+
+  useEffect(() => {
+    const shrinkHeader = () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
+        headerRef.current.classList.add("shrink");
+      } else {
+        headerRef.current.classList.remove("shrink");
+      }
+    };
+    window.addEventListener("scroll", shrinkHeader);
+    return () => {
+      window.removeEventListener("scroll", shrinkHeader);
+    };
+  }, []);
 
   return (
     <div ref={headerRef} className="header">
