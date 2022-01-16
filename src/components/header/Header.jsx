7d88ac1174a0d139b979f853322a1bfdd03cdc3d
Đinh Thanh Tú <89075138+Thanhtu179@users.noreply.github.com>
2022-01-16 15:36:30 +0900

    make header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,63 @@
+import React, { useRef, useEffect } from "react";
+
+import "./header.scss";
+import logo from "../../assets/tmovie.png";
+import { Link, useLocation } from "react-router-dom";
+
+const headerNav = [
+  {
+    display: "Home",
+    path: "/",
+  },
+  {
+    display: "Movies",
+    path: "/movie",
+  },
+  {
+    display: "TV Series",
+    path: "/tv",
+  },
+];
+
+useEffect(() => {
+  const shrinkHeader = () => {
+    if (
+      document.body.scrollTop > 100 ||
+      document.documentElement.scrollTop > 100
+    ) {
+      headerRef.current.classList.add("shrink");
+    } else {
+      headerRef.current.classList.remove("shrink");
+    }
+  };
+  window.addEventListener("scroll", shrinkHeader);
+  return () => {
+    window.removeEventListener("scroll", shrinkHeader);
+  };
+}, []);
+
+const Header = () => {
+  const { pathname } = useLocation();
+  const headerRef = useRef(null);
+  const active = headerNav.findIndex((e) => e.path === pathname);
+
+  return (
+    <div ref={headerRef} className="header">
+      <div className="header__wrap container">
+        <div className="logo">
+          <img src={logo} alt={logo} />
+          <Link to="/">CyberMovie</Link>
+        </div>
+        <ul className="header__nav">
+          {headerNav.map((e, i) => (
+            <li key={i} className={`${i === active ? "active" : ""}`}>
+              <Link to={e.path}>{e.display}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
